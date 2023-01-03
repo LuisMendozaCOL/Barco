df5f1a4f09c5b28a583e92d0cc0458dc6a739455
dannytaro <daniicampillo2@gmail.com>
2023-01-03 16:39:26 -0500

    nuevas versiones controladas por Bluetooth

--- led.js
+++ led.js
@@ -1,5 +1,5 @@
 var jf = require("johnny-five")
-var circuito = new jf.Board({ port: "COM7" });
+var circuito = new jf.Board({ port: "COM12" });
 
 circuito.on("ready", prender);
 
@@ -7,6 +7,5 @@ function prender() {
     // var led_amarillo = new jf.Led(13);
     var led_azul = new jf.Led(13);
     // led_amarillo.blink(25);
-    led_azul.blink(5);
-    console.log("Hola, bienvenido a Platzi")
+    led_azul.blink(2000);
 }
